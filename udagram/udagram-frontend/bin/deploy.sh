b5d0ab65233ad8d2e153a6c340849622ff40c5a1
Sarah <qsalahmadi@gmail.com>
2022-12-24 01:48:08 +0300

    update: package.json

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
+aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-ba68ade9-96b1-481e/ --profile default
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-ba68ade9-96b1-481e/ --profile default
